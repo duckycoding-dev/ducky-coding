c929ac1f82149f5346819b058c5f7d04f8fd8935
Ducky <duckycoding.dev@gmail.com>
2024-03-06 19:50:02 +0100

    add intellisense for env variables and custom relative path for astro package

--- packages/astro/src/env.d.ts
+++ packages/astro/src/env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="astro/client" />
+
+
+/**
+ * ===================================================
+ * ====== Enable intellisense for ENV Variables ======
+ * ===================================================
+ */
+
+interface ImportMetaEnv {
+  // env variables declared inside the .env file
+  // REMEMBER: only variables prefixed with PUBLIC_ are available on the client side
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
