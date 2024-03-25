5f42089a0e110aa40f41194a9a68fcea29a44c8a
Davide Milan <davide.milan@wavelop.com>
2024-03-25 15:55:17 +0100

    add tailwind 3.4, closes #2

--- packages/astro-app/src/env.d.ts
+++ packages/astro-app/src/env.d.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference path="../.astro/db-types.d.ts" />
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
 
 /**
  * ===================================================
