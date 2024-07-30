da292ecd0b5b556cf8a8c406f6fc7edd4a12431f
Davide Milan <davide.milan@wavelop.com>
2024-07-30 16:05:16 +0200

    add post content collection schema with example

---
+++ packages/astro-app/src/content/config.ts
@@ -0,0 +1,46 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from 'astro:content';
+
+// 2. Define your collection(s)
+const postsCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    summary: z.string().optional(),
+    content: z.string(),
+    authors: z
+      .array(
+        z.object({
+          name: z.string(),
+          id: z.string(),
+        }),
+      )
+      .min(1),
+    topic: z.object({
+      title: z.string(),
+      image: z.object({
+        fileName: z.string(),
+        alt: z.string(),
+      }),
+      slug: z.string(),
+    }),
+    tags: z.array(z.string()).min(1),
+    timeToRead: z.number(),
+    createdAt: z.date(),
+    publishedAt: z.date().optional(),
+    updatedAt: z.date().optional(),
+    bannerImage: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+    isPublished: z.boolean().default(false),
+    language: z.enum(['en', 'it']).default('en'),
+  }),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  posts: postsCollection,
+};
