e8c0f603cced2f9a4b5e6234e941ce62349aed32
Ducky <duckycoding.dev@gmail.com>
2024-08-09 01:08:42 +0200

    fix migration sql file

--- packages/db/src/migrations/0006_blushing_slapstick.sql
+++ packages/db/src/migrations/0006_blushing_slapstick.sql
@@ -4,4 +4,4 @@ ALTER TABLE `posts` ADD `summary` text NOT NULL;--> statement-breakpoint
 ALTER TABLE `posts` ADD `content` text NOT NULL;--> statement-breakpoint
 ALTER TABLE `posts` ADD `language` text NOT NULL;--> statement-breakpoint
 ALTER TABLE `posts` ADD `timeToRead` integer DEFAULT 1 NOT NULL;--> statement-breakpoint
-ALTER TABLE `posts` ADD `stat` text DEFAULT 'draft' NOT NULL;
+ALTER TABLE `posts` ADD `status` text DEFAULT 'draft' NOT NULL;
