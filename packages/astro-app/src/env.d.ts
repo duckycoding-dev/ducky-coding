ff3489c957cad8f454ef0544e76d657138a7ebee
Davide Milan <davide.milan@wavelop.com>
2025-01-20 17:22:04 +0100

    update astro to v5.1.17, update node version, fix all issues with major upgrade

--- packages/astro-app/src/env.d.ts
+++ packages/astro-app/src/env.d.ts
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
+/*
 /// <reference path="../.astro/actions.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+*/
 
 /**
  * ===================================================
