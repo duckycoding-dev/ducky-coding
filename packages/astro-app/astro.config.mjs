dd9f5d8b017c43930f493c3244f6df0798faa67f
Ducky <duckycoding.dev@gmail.com>
2025-05-14 19:35:07 +0200

    wip upgrades

--- packages/astro-app/astro.config.mjs
+++ packages/astro-app/astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
 import netlify from '@astrojs/netlify';
