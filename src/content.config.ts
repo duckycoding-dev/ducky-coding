bdee053ac7f41fac040f2e685f993cea7411547f
Ducky <duckycoding.dev@gmail.com>
2025-05-25 18:36:54 +0200

    upgrade deps and fix tsconfig include settings

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,20 +1,18 @@
 // 1. Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content';
+import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 import { PostContentSchema } from '@custom-types/entities';
 
-export type PostContent = z.infer<typeof PostContentSchema>;
-
 // 2. Define your collection(s)
-const postsCollection = defineCollection({
+const posts = defineCollection({
   loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/posts' }),
   schema: PostContentSchema,
 });
 
-const testCollection = defineCollection({
+const md = defineCollection({
   loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/md' }),
 });
 
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
-export const collections = { posts: postsCollection, md: testCollection };
+export const collections = { posts, md };
