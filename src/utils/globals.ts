8703409c6d927cbe22eec24ba12452c27d6b511d
duckycoding-dev <duckycoding.dev@gmail.com>
2025-06-08 18:50:05 +0200

    add first memes, and publish first blog post

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -12,3 +12,13 @@ export const WEBSITE_FIRST_PUBLISHING_DATE = new Date(
 ).toISOString();
 
 export const WEBSITE_ROOT = 'https://duckycoding.dev';
+
+export const MY_SOCIALS_LINKS = {
+  github: 'https://github.com/duckycoding-dev',
+  x: 'https://x.com/ducky_coding',
+  linkedin: 'https://www.linkedin.com/in/davide-m-997874254/',
+  instagram: 'https://www.instagram.com/ducky.coding/',
+  tiktok: 'https://www.tiktok.com/@ducky.coding',
+  youtube: 'https://www.youtube.com/@ducky.coding',
+  reddit: 'https://www.reddit.com/user/DuckyCodingDev/',
+} as const;
