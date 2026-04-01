aa297d88672b75b606316e744e2c0bd2adb9688e
duckycoding-dev <duckycoding.dev@gmail.com>
2026-04-01 17:58:19 +0200

    chore: add husky configs, commitlint and lintstaged

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,7 @@ export default defineConfig(
       '**/*.md',
       '**/*.mdx',
       '**/.vscode/**/*',
+      '**/.husky/**',
       '**/.astro/**',
       '**/.netlify/**',
       '**/.claude/**',
